a9ba081753cd52426a6065cb7c2df3ba2b03c6a6
JohnnTech <132626922+JohnnTech@users.noreply.github.com>
2023-09-11 14:02:37 -0300

    Initial commit

---
+++ teste.js
@@ -0,0 +1,9 @@
+var mostrarSite = true;
+const site = "www.teste.com";
+
+console.log("Hello World");
+console.log("Meu nome é Johnattan");
+
+if (mostrarSite){ // se mostrar site
+    console.log(site); // print site
+}
